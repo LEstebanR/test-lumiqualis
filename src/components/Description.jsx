baf41ee9a8a2005ee956d3a41550d3343f5fef69
Luis Esteban Ramírez <leramirezca@gmail.com>
2021-12-29 22:54:49 -0500

    add movies info styles

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -3,9 +3,18 @@ import { faImdb } from "@fortawesome/free-brands-svg-icons";
 
 
 const Description = ({picture, movie}) => {
-  console.log(movie);
+  
+  const background = movie ?  {
+    backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.poster_path})` ,
+    backgroundSize: "cover", 
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+
+
+  } : null
   return (
-    <div className="description-container">
+    
+    <div className="description-container" style={background}>
       {movie ? 
       <div className="movie-info">
         <h1>{movie.title}</h1>
@@ -22,18 +31,18 @@ const Description = ({picture, movie}) => {
           </tr>
           <tr>
             <td>{movie.release_date}</td>
-            <td><a href={`https://www.imdb.com/title/${movie.imdb_id}`} target="blank"><FontAwesomeIcon icon={faImdb}/></a></td> 
+            <td><a href={`https://www.imdb.com/title/${movie.imdb_id}`} target="blank"><FontAwesomeIcon icon={faImdb} className="icon"/></a></td> 
             <td>{movie.budget}</td>
             <td>{}</td>
             <td>{movie.revenue}</td>
             <td>{movie.status}</td>
           </tr>
-          <p>Rating: {movie.vote_average    }</p>
         </table>
+        <p>Rating: {movie.vote_average    }</p>
       </div>
       : <img src={picture} alt="NASA Pic of the day" className="picture-of-the-day"/>
     }
-     
+
     </div>
   )
 }
